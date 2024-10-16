1b46dc99182fbd6b1fd782fe11adaf9af769aa19
ltyCat <1009808178@qq.com>
2024-10-16 21:15:53 +0800

    feat: excel解析后分页展示

--- src/views/excel/upload-excel.vue
+++ src/views/excel/upload-excel.vue
@@ -1,22 +1,37 @@
 <template>
   <div class="app-container">
     <upload-excel-component :on-success="handleSuccess" :before-upload="beforeUpload" />
-    <el-table :data="tableData" border highlight-current-row style="width: 100%;margin-top:20px;">
+    <el-table v-show="tableDataByPage.length > 0" :data="tableDataByPage" border highlight-current-row style="width: 100%;margin-top:20px;">
+      <el-table-column type="index" />
       <el-table-column v-for="item of tableHeader" :key="item" :prop="item" :label="item" />
     </el-table>
+    <Pagination v-show="tableData.length" :total="tableData.length" :page.sync="pageNumber" :limit.sync="pageSize" />
   </div>
 </template>
 
 <script>
 import UploadExcelComponent from '@/components/UploadExcel/index.vue'
+import Pagination from '@/components/Pagination' // Secondary package based on el-pagination
 
 export default {
   name: 'UploadExcel',
-  components: { UploadExcelComponent },
+  components: { UploadExcelComponent, Pagination },
   data() {
     return {
       tableData: [],
-      tableHeader: []
+      tableHeader: [],
+      pageSize: 10,
+      pageNumber: 1
+    }
+  },
+  computed: {
+    // 分页表格数据
+    tableDataByPage() {
+      console.log(this.pageNumber, 'pageNumber')
+      if (!this.tableData.length) return []
+      const res = this.tableData.slice((this.pageNumber - 1) * this.pageSize, this.pageSize * this.pageNumber)
+      console.log(res, 'res')
+      return res
     }
   },
   methods: {
@@ -34,6 +49,7 @@ export default {
       return false
     },
     handleSuccess({ results, header }) {
+      console.log(results)
       this.tableData = results
       this.tableHeader = header
     }
